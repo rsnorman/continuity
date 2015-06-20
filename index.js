7b5c0d8d42676b2f7776a0b44836468219d1cbe5
Ryan Norman <rsnorman15@gmail.com>
2015-06-20 01:24:24 -0700

    Add feature to chain promise functions sequentially

--- index.js
+++ index.js
@@ -6,12 +6,60 @@
 
 'use strict';
 
+var Continuity = function(collection, promiseFn) {
+  var _continuity, _promise, reject, resolve, progressCallbacks;
+  progressCallbacks = [];
+
+  function collectionIterator(collection, promiseFn, values) {
+    collection = Array.prototype.slice.call(collection);
+    values = Array.prototype.slice.call(values);
+
+    promiseFn(collection[0])
+      .then(function(value) {
+        collection.shift();
+        values.push(value);
+
+        progressCallbacks.map(function(callback) {
+          callback(value, values, values.length);
+        });
+
+        if ( collection.length > 0 ) {
+          collectionIterator(collection, promiseFn, values);
+        } else {
+          resolve(values);
+        }
+
+      })
+      .catch(reject);
+  }
+
+  _promise = new Promise(function(_resolve, _reject) {
+    resolve = _resolve;
+    reject = _reject;
+    collectionIterator(collection, promiseFn, []);
+  });
+
+  _continuity = {
+    then: function(callback) {
+      _promise.then(callback);
+      return _continuity;
+    },
+    catch: function(callback) {
+      _promise.catch(callback);
+      return _continuity;
+    },
+    progress: function(callback) {
+      progressCallbacks.push(callback);
+      return _continuity;
+    }
+  };
+
+  return _continuity;
+
+};
+
 /**
  * Module exports
  * @public
  */
-module.continuity = Continuity
-
-Continuity = function() {
-
-};
+module.exports = Continuity;
