32dd45251cd70799b8c92d5143b3dfc8c9045a6b
rsnorman <rsnorman15@gmail.com>
2015-06-19 23:05:34 -0700

    Initial commit of continuity

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * continuity
+ * Copyright(c) 2015 Ryan Scott Norman
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module exports
+ * @public
+ */
+module.continuity = Continuity
+
+Continuity = function() {
+
+};
