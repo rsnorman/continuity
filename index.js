e9271116146d6a0e4c0eb8e6461dd28d5bdac402
Ryan Norman <rsnorman15@gmail.com>
2015-06-20 10:03:02 -0700

    Documented Continuity object

--- index.js
+++ index.js
@@ -6,10 +6,61 @@
 
 'use strict';
 
+/**
+ * Creates an object that iterates over a collection and passing values
+ * into asynchronous functions that returns promises. Each function must wait
+ * until the previous one has finished before starting.
+ *
+ * The `then` method will return all the values resolved by the promises:
+ *
+ *       new Continuity([1, 2], function(value) {
+ *         new Promise(function(resolve) {
+ *           resolve(value + 1);
+ *         });
+ *       }).then(function(values) {
+ *         assert(values == [2, 3]);
+ *       });
+ *
+ * The `progress` method will return the value resolved by the current
+ * executing promise along with all the returned values and progress:
+ *
+ *       new Continuity([1, 2], function(value) {
+ *         new Promise(function(resolve) {
+ *           resolve(value + 1);
+ *         });
+ *       }).progress(function(value, values, progress) {
+ *         assert(value == 2 || value == 3);
+ *         assert(values == [2] || values == [2, 3]);
+ *         assert(progress == 1 || progress == 2);
+ *       });
+ *
+ * The `catch` method behaves like a Promise in that it returns the object that
+ * that caused the promise to reject.
+ *
+ * @param {Array} Array-like object that will be used to call promise returning function
+ * @param {Function} function that returns promises and will be called with array values
+ * @return {Continuity} for thenable methods and progress callback
+ * @public
+ */
 var Continuity = function(collection, promiseFn) {
-  var _continuity, _promise, reject, resolve, progressCallbacks;
+  var continuity,
+      promise,
+      reject,
+      resolve,
+      progressCallbacks;
+
+  // Empty array of progress callbacks
   progressCallbacks = [];
 
+
+  /**
+   * Recursive function that queues up promise functionswith values in collection
+   *
+   * @param {Array} Array-like object that will be used to call promise returning function
+   * @param {Function} function that returns promises and will be called with array values
+   * @param {Array} values that have been returned from promises
+   * @private
+   */
   function collectionIterator(collection, promiseFn, values) {
     collection = Array.prototype.slice.call(collection);
     values = Array.prototype.slice.call(values);
@@ -19,13 +70,18 @@ var Continuity = function(collection, promiseFn) {
         collection.shift();
         values.push(value);
 
+        // fire all progress callbacks on each iteration
         progressCallbacks.map(function(callback) {
           callback(value, values, values.length);
         });
 
+        // Still have more to run, execute another promise function
         if ( collection.length > 0 ) {
           collectionIterator(collection, promiseFn, values);
-        } else {
+        }
+
+        // All done, resolve promise
+        else {
           resolve(values);
         }
 
@@ -33,28 +89,39 @@ var Continuity = function(collection, promiseFn) {
       .catch(reject);
   }
 
-  _promise = new Promise(function(_resolve, _reject) {
+
+  /**
+   * Create promise to resolve once all other promises are resolved.
+   * Store resolve and reject functions so we can chain with a Promise-like
+   * object.
+   */
+  promise = new Promise(function(_resolve, _reject) {
     resolve = _resolve;
     reject = _reject;
     collectionIterator(collection, promiseFn, []);
   });
 
-  _continuity = {
+
+  /**
+   * Returns Promise-like object that allows thenable chains along with
+   * progress callbacks that fire on each iteration.
+   */
+  continuity = {
     then: function(callback) {
-      _promise.then(callback);
-      return _continuity;
+      promise.then(callback);
+      return continuity;
     },
     catch: function(callback) {
-      _promise.catch(callback);
-      return _continuity;
+      promise.catch(callback);
+      return continuity;
     },
     progress: function(callback) {
       progressCallbacks.push(callback);
-      return _continuity;
+      return continuity;
     }
   };
 
-  return _continuity;
+  return continuity;
 
 };
 
